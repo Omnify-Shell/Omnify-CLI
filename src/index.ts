c2a59de31dd25e5b80851543f77222caffd8c925
kush kumar <kushkumarking098765@gmail.com>
2026-05-08 21:36:03 +0530

    refactor: clean up index.ts formatting and remove unnecessary lines

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,27 @@
-import { program } from "commander";
-
-
+#!/usr/bin/env node
+import { program } from 'commander';
 
 program
-  .name("Omnify")
-  .description("Give native powers to your Web Application , Build with Omnify Shell")
-  .version("1.0.0")
-  .addHelpText('after', `
+  .name('Omnify')
+  .description(
+    'Give native powers to your Web Application , Build with Omnify Shell',
+  )
+  .version('1.0.0')
+  .addHelpText(
+    'after',
+    `
 Examples:
   npx omnify init
   npx omnify add camera
   npx omnify build --windows
   npx omnify login
   npx omnify publish
-  `)
+  `,
+  );
 export default program;
 
-program
-  .command("init")
-  .action(()=> console.log("hello init"));
+program.command('init').action(() => console.log('hello init'));
 
-
-program
-    .command("add")
-    .action(()=> console.log("added successfully"))
-
-
-
+program.command('add').action(() => console.log('added successfully'));
 
 program.parse();
